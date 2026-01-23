89b98ec54b60072cf26b7ecd0a00c840ae03c5e9
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-01-23 21:05:17 +0200

    printing the number of rows in a df

--- first-day-of-week-project/practice.py
+++ first-day-of-week-project/practice.py
@@ -3,7 +3,14 @@
 import pandas as pd
 
 df = pd.read_csv('first-day-of-week.csv')
-print(df.head(10))
+# print(df.head(10))
+
+# displaying column names and number of rows
+print(df.columns)
+
+# printing the number of rows in a df
+print(df.shape)
+print(df.shape[0])
 
 
 
