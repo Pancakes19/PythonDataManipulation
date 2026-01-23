c9e6f1849aaaabf4641f58f919822d41ce5a0f88
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-01-23 21:23:58 +0200

    counting the number of unique values in a column

--- first-day-of-week-project/practice.py
+++ first-day-of-week-project/practice.py
@@ -6,11 +6,16 @@ df = pd.read_csv('first-day-of-week.csv')
 # print(df.head(10))
 
 # displaying column names and number of rows
-print(df.columns)
+# print(df.columns)
 
 # printing the number of rows in a df
-print(df.shape)
-print(df.shape[0])
+# print(df.shape)
+# print(df.shape[0])
 
+# finding unique values in a column
+# the first shows the possible diff values
+# and the second shows the number of unique values
+print(df['first_day'].unique())
+print(df['first_day'].nunique())
 
 
