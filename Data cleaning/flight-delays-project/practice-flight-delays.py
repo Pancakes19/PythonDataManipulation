a89b67db25c44b1e638c66799b193d83273dae4d
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-02-17 20:38:07 +0200

    plotting data for flights

--- Data cleaning/flight-delays-project/practice-flight-delays.py
+++ Data cleaning/flight-delays-project/practice-flight-delays.py
@@ -22,8 +22,32 @@ departures['actual'] = pd.to_datetime(departures['actual'])
 # flights.loc[:, 'actual'] = pd.to_datetime(flights['actual'])
 
 # calcualting the delays
-departures['delay'] = departures.eval('actual - scheduled')
+# departures['delay'] = departures.eval('actual - scheduled')
+departures['delay'] = departures['actual'] - departures['scheduled']
 print(departures)
 
+# checking which flights are late with using dt.total_seconds()
+# Create a boolean column that marks flights as late (True if delay > 15 minutes / 900 seconds)
 
+departures['is_late'] = departures['delay'].dt.total_seconds() > 900
+# print(departures)
+
+# getting the day of the week
+departures['day_name'] = departures['actual'].dt.strftime('%a')
+print(departures)
+
+# grouping the df by day of the week and calculating the mean
+# of is_late for each day and converting to percentage
+proportion_delayed = departures.groupby('day_name')['is_late'].mean()
+percent_delayed = proportion_delayed * 100
+# print(percent_delayed)
+
+# changing the index
+new_index_order = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+percent_delayed = percent_delayed.reindex(new_index_order)
+# print(percent_delayed)
+
+plt.bar(percent_delayed.index, percent_delayed)
+plt.ylabel('Percent Delayed')
+plt.show()
 
