f5637f09315895eed609c89a95b5b4450429582b
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-02-24 19:35:51 +0200

    finding higest value with colum

--- Data-visualization/deepest-divers-project/oceanDive.py
+++ Data-visualization/deepest-divers-project/oceanDive.py
@@ -1,1 +1,16 @@
 # ocean's deepest divers
+import pandas as pd
+import matplotlib.pyplot as plt
+
+divers = pd.read_csv('deepest-diving-animals.csv')
+# print(divers)
+
+# finding lowest diver in three ways
+divers = divers.query('depth == @divers["depth"].max()')
+
+divers[divers['depth'] == divers['depth'].max()]
+
+divers.loc[divers['depth'].idxmax()]
+
+
+print(divers)
