f98be7273c2adaf753dbddc14dd1bb4a0183f3df
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-06-23 15:00:18 +0200

    added more lines of code for data

--- Data Modelling/tusked-elephants-project/elephants.py
+++ Data Modelling/tusked-elephants-project/elephants.py
@@ -2,6 +2,8 @@ import os
 os.system('cls')
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 
 df = pd.read_csv('male-elephant-tusk-size.csv')
 #print(df.head(10))
@@ -19,8 +21,22 @@ post_recovery = df.query('period == "2005-13"')
 
 # printing the avarage for before and after 
 
-print('Before: ', pre_poaching['tusk_length'].mean())
-print('After: ', post_recovery['tusk_length'].mean())
+#print('Before: ', pre_poaching['tusk_length'].mean())
+#print('After: ', post_recovery['tusk_length'].mean())
+
+#ploting a scatter plot for tusk length and a shoulder and formating the plot
+plt.figure(figsize=(6, 4))
+plt.scatter(pre_poaching['shoulder_height'],
+            pre_poaching['tusk_length'], marker='^')
+plt.scatter(post_recovery['shoulder_height'], post_recovery['tusk_length'], marker='s')
+plt.xlabel('Shoulder height (cm)')
+plt.ylabel('Tusk Length (cm)')
+plt.text(x=200, y=120, s='Pre-poaching', color='C0')
+plt.text(x=200, y=35, s='Pre-poaching', color='C1')
+plt.show()
+
+#modelling tusk length
+
 
 
 
